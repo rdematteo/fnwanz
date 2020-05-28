c91371ef0d8d8a31e1e30418310e9636319e2809
Robert De Matteo <robertdematteo@hotmail.com>
2020-05-28 16:28:24 +1000

    add contents to homepage

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Routes from "./Routes";
 import { Title } from "../src/text/text";
 import styled from "styled-components";
 
-const Heading = styled.h1`
-  background-color: yellow;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  // background-color: red;
 `;
 
-console.log(Title);
+const Heading = styled.h1`
+  background-color: white;
+  text-align: center;
+  padding: 2rem;
+  @media (max-width: 768px) {
+    background-color: yellow;
+    font-size: 18px;
+  }
+`;
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Navbar />
-        <Heading>{Title}</Heading>
-
+        <Content>
+          <Heading>{Title}</Heading>
+        </Content>
         <Routes />
       </>
     );
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Intro } from "../../text/text";
+import { Intro, AimTitle, AimText, Aim } from "../../text/text";
+import styled from "styled-components";
+
+const Para = styled.div`
+  padding: 1rem 2rem;
+
+  @media (max-width: 768px) {
+    background-color: yellow;
+    font-size: 12px;
+    padding: 10px;
+  }
+`;
 
 class HomePage extends React.Component {
-  state = {};
-
   render() {
-    return <>{Intro}</>;
+    return (
+      <>
+        <Para>{Intro}</Para>
+        <Para>{AimTitle}</Para>
+        <Para>{AimText}</Para>
+        <Para>{Aim}</Para>
+      </>
+    );
   }
 }
 
